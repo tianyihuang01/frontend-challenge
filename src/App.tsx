8480da0081729500171a6c6682f8a40e1ad384e8
Jack Cuthbert <jckcthbrt@gmail.com>
2021-07-07 17:53:08 +1000

    Add Pokemon interfaces and data import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import 'css-wipe'
+import 'css-wipe';
+import jsonData from './data/pokemon-gen1-gen2-gen3.json';
+import type { Pokemon } from './types';
+const data: Pokemon[] = jsonData as Pokemon[];
 
 function App() {
   return (
---
+++ src/types.ts
@@ -0,0 +1,38 @@
+export type StatName =
+	| "hp"
+	| "attack"
+	| "defense"
+	| "speed"
+	| "special-attack"
+	| "special-defense";
+
+export interface PokemonStat {
+	base_stat: number;
+	effort: number;
+	stat_name: StatName;
+}
+
+export interface PokemonType {
+	slot: number;
+	type_name: string;
+}
+
+export interface PokemonSprite {
+	artwork_url: string;
+	front_url: string;
+	back_url: string;
+}
+
+export interface Pokemon {
+	id: number;
+	order: number;
+	name: string;
+	height: number;
+	genus: string;
+	habitat: string;
+	color: string;
+	flavor_text: string;
+	stats: PokemonStat[];
+	sprite: PokemonSprite;
+	types: PokemonType[];
+}
