67dbb5b34ac931695723ca5a111b2b15cec29b8f
Jack Cuthbert <jckcthbrt@gmail.com>
2023-03-02 12:11:06 +1100

    chore(deps): update to latest versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
 import 'css-wipe';
 import jsonData from './data/pokemon-gen1-gen2-gen3.json';
 import type { Pokemon } from './types';
-const data: Pokemon[] = jsonData as Pokemon[];
+
+const data = jsonData as Pokemon[];
 
 function App() {
-  return (
-		<>
-			<h1>It works!</h1>
-		</>
-  );
+	return <h1>It works!</h1>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import 'css-wipe'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
